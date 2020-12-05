7dff965f1f2da261f633d3ddc75d8696ff77ae2c
Kdoggg666 <kenanwright@gmail.com>
2020-12-05 22:22:19 +0000

    Add js file and event listeners

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("h3").css("text-decoration", "underline");
+});
+
+
+
+$("#testID").click(function() {
+    $(".custom-div-1").css("background-color", "red");
+});
+
